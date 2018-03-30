03359bba2466c0596f1758f56426561394e1a06a
James Munns <james.munns@gmail.com>
2018-03-30 14:09:37 +0200

    Add Xargo to rebuild std with -Oz

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
-#![feature(global_allocator)]
-#![feature(allocator_api)]
-
-// This section is necessary after rustc 1.20.x due to the new way
-// allocator selection is handled. Either `jemalloc` may be used, or the system
-// allocator (`malloc`) provided by glibc
-#[cfg(feature = "system-alloc")]
-mod allocator {
-    use std::heap::System;
-
-    #[global_allocator]
-    pub static mut THE_ALLOC: System = System;
-}
-
-#[cfg(not(feature = "system-alloc"))]
-mod allocator {
-    #[allow(dead_code)]
-    pub static THE_ALLOC: () = ();
-}
-
-#[allow(unused_imports)]
-use allocator::THE_ALLOC;
 
 extern crate rocket;
 
